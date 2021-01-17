60b99645b9354e6e08ba1e1e9608612c59efd41f
Max <acad.teh@gamil.com>
2021-01-18 00:29:37 +0500

    default start

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import 'materialize-css/dist/js/materialize.min'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
@@ -9,5 +10,5 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount('#app')
